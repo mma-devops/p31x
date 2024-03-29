b0f380297d468c832207dc9402fcc4d2fe9a7d82
mma-devops <mironovmatvey4444@gmail.com>
2024-03-29 18:54:02 +0300

    xdxd

--- p312.cpp
+++ p312.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-int main(){
-    cout << "xd";
+int main()
+{
+    // прога угадай число с жЫзнями))
+    srand(time(NULL));
+    int num, secretNum = rand() % 10 + 1;
+    for(int i = 3; i > 0; i--){
+        cin >> num;
+        if (num == secretNum){
+            cout << "\nПАБЕДА))0";
+            break;
+        }
+        else{
+            printf("NE)O)), осталось %d жизней.\n", i-1);
+            continue;
+        }
+    }
+    cout << secretNum << endl;
+    cout << "\nGAEM OWER)))000нулик";
+    
 }
